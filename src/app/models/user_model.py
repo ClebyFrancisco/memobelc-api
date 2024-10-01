4d6c9b95372531623b0d10672272ee9ce5fbd08c
Cleby Francisco <cfs7@softex.cin.ufpe.br>
2024-10-01 17:06:42 -0300

    feat: verify_is_confirmed

--- src/app/models/user_model.py
+++ src/app/models/user_model.py
@@ -7,12 +7,14 @@ class UserModel:
         self.email = email
         self.password = password
 
+
     def save_to_db(self):
         """Salva o usuário no banco de dados MongoDB"""
         user_data = {
             'name': self.name,
             'email': self.email,
-            'password': self.password
+            'password': self.password,
+            'is_confirmed': False
         }
         return mongo.db.users.insert_one(user_data)
 
@@ -31,11 +33,33 @@ class UserModel:
         if user_data:
             return UserModel(**user_data)
         return None
+    
+    @staticmethod
+    def verify_is_confirmed(email):
+        """Verificar se um usuário está confirmado!"""
+        user_data = mongo.db.users.find_one({'email': email})
+        print(user_data)
+        if user_data and user_data['is_confirmed']:
+            return True
+        else:
+            return False
 
-    def update_password(self, new_password):
-        """Atualiza a senha do usuário"""
-        self.password = new_password
-        mongo.db.users.update_one({'id': self.id}, {'$set': {'password': new_password}})
+    
+    @staticmethod
+    def turn_confirmed(email):
+        """Alterar o status de um usuário para confirmado"""
+        mongo.db.users.update_one({'email': email}, {'$set': {'is_confirmed': True}})
+        
+        
+
+    def update_password(self, user_id,  new_password):
+        user_data = delf.find_by_id(user_id)
+        
+        if user_data:
+            return UserModel(**user_data)
+        return None
+    
+        mongo.db.users.update_one({'id': user_data.id}, {'$set': {'password': new_password}})
 
     def to_dict(self):
         """Converte o objeto UserModel para dicionário"""
--- src/app/services/auth_service.py
+++ src/app/services/auth_service.py
@@ -25,10 +25,20 @@ class AuthService:
                 'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=72)
             }, current_app.config['SECRET_KEY'], algorithm="HS256")
             
-            if current_app.config['FLASK_ENV'] == 'development':
-                return {'token': token, 'name':user.name, 'email':user.email}
+            is_confirmed = UserModel.verify_is_confirmed(user.email)
+            if is_confirmed:
+                if current_app.config['FLASK_ENV'] == 'development':
+                    return {'token': token, 'name':user.name, 'email':user.email}
+                else:
+                    return LoginResponse(token=token, name=user.name, email=user.email)
             else:
-                return LoginResponse(token=token, name=user.name, email=user.email)
+                if current_app.config['FLASK_ENV'] == 'development':
+                    return {'pending': "O Usuário não está confirmado!" }
+                else:
+                    return ErrorResponse(erro="O Usuário não está confirmado!")
+                
+            
+
             
         return None
     def refresh_token(self, token):
@@ -49,6 +59,18 @@ class AuthService:
             return LoginResponse(token=token, name=user.name, email=user.email)
             
         return None
+    
+        def confirm_email(token):
+            try:
+                email = s.loads(token, salt='email-confirm', max_age=3600)  # O token expira em 1 hora
+                user = db.users.find_one({'email': email})
+                if user and not user['is_confirmed']:
+                    db.users.update_one({'email': email}, {'$set': {'is_confirmed': True}})
+                    return jsonify({"message": "Email confirmado com sucesso."}), 200
+                else:
+                    return jsonify({"message": "Email já confirmado ou inválido."}), 400
+            except Exception as e:
+                return jsonify({"message": "Token inválido ou expirado."}), 400
     
     def updated_password(self, _id, new_password):
         pass
